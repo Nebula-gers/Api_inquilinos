8587d9011512400751e820512dcfeefe35bc9698
edwin <edwin.11047@gmail.com>
2025-02-12 22:07:19 -0600

    Updated files

--- src/main/java/com/reservas/InquilinosApi/controller/AlquilerController.java
+++ src/main/java/com/reservas/InquilinosApi/controller/AlquilerController.java
@@ -1,6 +1,7 @@
 package com.reservas.InquilinosApi.controller;
 
-import com.reservas.InquilinosApi.dto.PropiedadDto;
+import com.reservas.InquilinosApi.dto.PropiedadDTO;
+import com.reservas.InquilinosApi.model.Inquilino;
 import com.reservas.InquilinosApi.model.Propiedad;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AlquilerController {
 
-    @GetMapping("/propiedad/{id}")
+    @GetMapping ("/propiedad/{id}")
     @ResponseBody
-    public PropiedadDto devolverPropiedad(@PathVariable Long id){
-        
+    public PropiedadDTO devolverPropiedad (@PathVariable Long id) {
+        //que a través de la id buscamos la propiedad
+        //trajimos al inquilino asociado a esa propiedad
+        Propiedad prop = new Propiedad(15487L, "Casa", "308 Negro Arroyo Lane", 200.0, 40000.0);
+        Inquilino inqui = new Inquilino(1L, "12345678", "Walter", "White", "Profesor de química");
+
+        PropiedadDTO propiDTO = new PropiedadDTO();
+        //asignamos los datos que necesitamos de propiedad
+        propiDTO.setId_propiedad(prop.getId_propiedad());
+        propiDTO.setTipo_propiedad(prop.getTipo_propiedad());
+        propiDTO.setDireccion(prop.getDireccion());
+        propiDTO.setValor_alquiler(prop.getValor_alquiler());
+        //asignamos los datos que necesitamos de Inquilino
+        propiDTO.setNombre(inqui.getNombre());
+        propiDTO.setApellido(inqui.getApellido());
+
+        return propiDTO;
     }
+
 }
